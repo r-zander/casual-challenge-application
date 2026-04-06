c49ee6c48d55edeef0112091f431a2b8b5dc3fd8
JanikNissen <nissenjanik@gmail.com>
2026-04-06 12:38:12 +0200

    Added migrations for newest season (20)

---
+++ src/main/resources/db/changelog/migrations/20260406_1106_00_add_season_20.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset janik_nissen:20260406_0005_00_add_season_20.sql
+UPDATE public.season
+    SET end_date = '2026-05-04'
+    WHERE id = 19;
+INSERT INTO public.season (id, season_number, start_date, end_date, updated_at)
+VALUES
+    (20, 20, '2026-04-05', '2026-06-07', now());
